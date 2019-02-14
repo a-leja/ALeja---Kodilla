cac6cd4d2d56307071ea6d069ada907edd8be32c
Anna Leja <anna.leja16@gmail.com>
2019-02-14 23:40:58 +0100

    mocked statistics

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/com/kodilla/testing/forum/statistics/Statistics.java
@@ -0,0 +1,61 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.List;
+
+public interface Statistics {
+    List<String> usersNames(); // list of users names
+    int postsCount();          // total quantity of forum posts
+    int commentsCount();       // total quantity of forum comments
+}
+
+public class Statstics {
+
+    public int getUsersQuantity() {
+
+        return;
+    }
+
+
+    public int getPostsQuantity() {
+
+        return;
+    }
+
+
+    public int getCommentsQuantity() {
+
+        return;
+    }
+
+
+    public int getAveragePostsQuantity() {
+
+        return;
+    }
+
+
+
+    public int getAverageCommentsQuantity() {
+
+        return;
+    }
+
+
+    public int getAverageCommentsPerPost/() {
+
+        return;
+    }
+
+
+    public int calculateAdvStatistics(Statistics statistics) {
+
+        return;
+    }
+
+
+    public int showStatistics() {
+
+        return;
+    }
+}
+
