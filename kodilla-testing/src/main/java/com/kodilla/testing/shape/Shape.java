a0211fc3a198b8c01420d524eb40e27e5fb132e3
Anna Leja <anna.leja16@gmail.com>
2019-02-14 23:16:23 +0100

    stub and Mock

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/Shape.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Shape.java
@@ -1,8 +1,8 @@
 package com.kodilla.testing.shape;
 
 interface Shape {
-    private String name;
-    private double field;
+    String name;
+    double field;
 
     public Shape(String name, double field) {
         this.name = name;
