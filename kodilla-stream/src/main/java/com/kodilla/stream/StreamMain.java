f025b4a24cc8db3639fc83c4883e33d8b35c99de
Anna Leja <anna.leja16@gmail.com>
2019-02-17 02:42:12 +0100

    lambda package added

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,12 @@
 package com.kodilla.stream;
 
-public class StreamMain {
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Processor;
 
+public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        Processor processor = new Processor();
+        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        processor.execute(executeSaySomething);
     }
 }
