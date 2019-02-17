f7c75ade105a7463226104e1171604713a679dd2
Anna Leja <anna.leja16@gmail.com>
2019-02-17 22:56:01 +0100

    immutable class ForumUser added

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 import com.kodilla.stream.reference.Styles;
@@ -41,5 +42,9 @@ public class StreamMain {
         poemBeautifier.beautify("Fifth sample text", Styles::checkTextLength);
         poemBeautifier.beautify("Sixth sample text", Styles::convertTheStringToLowerCaseLetters);
 
+
+        //Stream sample exercise
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
