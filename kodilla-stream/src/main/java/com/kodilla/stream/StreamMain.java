43571be0089eced77e2cb1842a746aaf65cc7357
Anna Leja <anna.leja16@gmail.com>
2019-02-18 15:26:19 +0100

    BookDirectory Stream added

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,8 +3,15 @@ package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
 import com.kodilla.stream.reference.Styles;
+//
+
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.book.Book;
+
+
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -46,5 +53,33 @@ public class StreamMain {
         //Stream sample exercise
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
+
+
+
+        People.getList().stream()
+                .map(s -> s.toUpperCase())
+                .forEach(System.out::println)
+
+
+
+        //Kaskadowe łączenie operacji na Stream
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+
+
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+
+        }
+
+
+
     }
 }
