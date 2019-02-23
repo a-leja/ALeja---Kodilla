888ad422afefdd2367168e4771ea530bfd2dd636
Anna Leja <anna.leja16@gmail.com>
2019-02-23 01:03:54 +0100

    count the average using funcional for loop

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,8 +1,21 @@
 package com.kodilla.stream.array;
 
-public interface ArrayOperations {
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
 
-    static getAverage(int[] numbers) {
-        return;
+
+interface ArrayOperations {
+    static double getAverage(int[] numbers) {
+
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .forEach(System.out::println);
+
+
+        OptionalDouble result = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average();
+
+        return result.getAsDouble();
     }
 }
