bb0e14dcc6b4239d6ab4e4734eb1ec1ebc83c116
Anna Leja <anna.leja16@gmail.com>
2019-02-23 03:14:46 +0100

    add FileReader

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,6 +1,7 @@
 package com.kodilla.exception.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,9 +14,17 @@ public class FileReader {
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
-        Path path = Paths.get(file.getPath());
-        Stream<String> fileLines = Files.lines(path);
 
-        System.out.print(file.getPath());
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+
+            fileLines.forEach(System.out::println);
+
+        } catch (IOException e) {
+
+            System.out.print("Oh no! Something went wrong! Error: " + e);
+        } finally {
+
+            System.out.print("Trial no.: ");
+        }
     }
 }
