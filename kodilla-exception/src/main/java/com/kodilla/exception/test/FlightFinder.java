46b17b9946812e74b80d37a55bfc543d7feb75ab
Anna Leja <anna.leja16@gmail.com>
2019-02-28 18:48:16 +0100

    amend findAirport method

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -6,24 +6,27 @@ import java.util.Map;
 
 public class FlightFinder {
 
-    Map<String, Boolean> routes = new HashMap<>();
-
-    //adding airports to the map of available airports
-    routes.put("KRK", true);
-    routes.put("ALC", false);
-    routes.put("OPO", true);
-    routes.put("LON", false);
-    routes.put("NYC", true);
-    routes.put("HEL", false);
 
 
     public boolean findFlight(Flight flight) throws RouteNotFoundException {
         return findAirport(flight.getDepartureAirport()) && findAirport(flight.getArrivalAirport());
+
     }
 
-    public boolean findAirport(String airport) {
+    public boolean findAirport(String airport) throws RouteNotFoundException {
+
+        Map<String, Boolean> routes = new HashMap<>();
+        //adding airports to the map of available airports
+        routes.put("KRK", true);
+        routes.put("ALC", false);
+        routes.put("OPO", true);
+        routes.put("LON", false);
+        routes.put("NYC", true);
+        routes.put("HEL", false);
+
+
         if (routes.containsKey(airport)) {
-            routes.get(airport);
+           return routes.get(airport);
         } else {
             throw new RouteNotFoundException("Given airport does not exist!");
         }
