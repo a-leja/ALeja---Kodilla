ae2182a762615109ac243bae2d1887b3cd86bccd
Anna Leja <anna.leja16@gmail.com>
2019-04-25 18:36:55 +0200

    delete orderProcessor class

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/Food2Door.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/Food2Door.java
@@ -1,7 +1,12 @@
 package com.kodilla.good.patterns.challenges.Food2Door;
 
 public class Food2Door {
+    public static void main(String[] args) {
 
-    OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
-    OrderRequest orderRequest = orderRequestRetriever.retrieve();
+        OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
+        OrderRequest orderRequest = orderRequestRetriever.retrieve();
+
+
+        orderRequestRetriever.processOrder(orderRequest);
+    }
 }
