3dcc363656067b206c1fffaacbce1dc29bb5dcce
Anna Leja <anna.leja16@gmail.com>
2019-05-18 14:33:30 +0200

    add Item controller

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/dao/ItemDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/dao/ItemDao.java
@@ -1,14 +1,15 @@
 package com.kodilla.hibernate.invoice.dao;
 
-import com.kodilla.hibernate.invoice.Product;
+import com.kodilla.hibernate.invoice.Item;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import javax.transaction.Transactional;
 import java.util.List;
 
+
 @Transactional
 @Repository
-public interface ProductDao extends CrudRepository<Product, Integer> {
-        List<Product> findByProductName(String name);
+public interface ItemDao extends CrudRepository<Item, Integer> {
+    List<Item> findByItemName(String name);
 }
